7c4c2acca86668be0f807f1716cceefd3915bcf4
Mandar Haldekar <mandar.haldekar@gmail.com>
2015-03-14 17:52:45 -0400

    Implemented detectActivity method for walking

--- app/src/main/java/com/mobilecomputing/mandarandash/activityrecongnition/DataCollectorService.java
+++ app/src/main/java/com/mobilecomputing/mandarandash/activityrecongnition/DataCollectorService.java
@@ -7,25 +7,37 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 
 public class DataCollectorService extends Service implements SensorEventListener {
 
+    private static final long INTERVAL = 1*30*1000 ;
     private SensorManager sensorManager;
     private Sensor accelerometer, gyroscope;
     private float xAccl, yAccl, zAccl;
     private IBinder myBinder = new LocalBinder();
     private Handler myHandler = new Handler();
-    private float sensorData[];
+    private float sensorData[],gravity[],linear_acceleration[];
     private ArrayList<SensorDataModel> accelerometer_data; //This will hold accelerometer data
-    private long currentTime;
+    private long timeOutInterval;
+    private ArrayList<ActivityRecord> activityRecordArrayList;
+    private Integer count=0;
+
     public DataCollectorService() {
-        sensorData = new float[3];
+
     }
 
     @Override
@@ -36,39 +48,56 @@ public class DataCollectorService extends Service implements SensorEventListener
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+
         synchronized (this) {
 
-            if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            {
+            if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                 /**
                  * TO-DO: Collect data continuously and at the end of two min interval, call the algorithm to detect activity
                  */
-                if(currentTime > System.currentTimeMillis() ) {
+                count++;
+                if (timeOutInterval > System.currentTimeMillis()) {
                     //Add reading to the results
 
-                    xAccl = event.values[0];
-                    yAccl = event.values[1];
-                    zAccl = event.values[2];
-                    sensorData[0] = xAccl;
-                    sensorData[1] = yAccl;
-                    sensorData[2] = zAccl;
-                    SensorDataModel sensorDataModel = new SensorDataModel(xAccl,yAccl,zAccl);
+                    //Filtering
+                    final float alpha = 0.8f;
+
+                    // Isolate the force of gravity with the low-pass filter.
+                    gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+                    gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+                    gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+                    // Remove the gravity contribution with the high-pass filter.
+                    linear_acceleration[0] = event.values[0] - gravity[0];
+                    linear_acceleration[1] = event.values[1] - gravity[1];
+                    linear_acceleration[2] = event.values[2] - gravity[2];
+
+                    //Magnitude
+                    linear_acceleration[3] = (float) Math.sqrt(linear_acceleration[0]*linear_acceleration[0] + linear_acceleration[1]*linear_acceleration[1] + linear_acceleration[2]*linear_acceleration[2]);
+
+
+                    SensorDataModel sensorDataModel = new SensorDataModel(linear_acceleration[0],linear_acceleration[1],linear_acceleration[2],linear_acceleration[3]);
+
                     accelerometer_data.add(sensorDataModel);
 
-                } else{
+                } else {
                     /**
                      * TO-DO: Call activity recognition algorithm here. Pass accelerometer_data arraylist and display activity on Main UI
                      */
+                    detectActivity(timeOutInterval - INTERVAL);
                     accelerometer_data = new ArrayList<SensorDataModel>();
-                    currentTime = System.currentTimeMillis() + 1*30*1000;
+                    timeOutInterval = System.currentTimeMillis() + INTERVAL;
                     myHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getApplicationContext(),"Returning sensor data",Toast.LENGTH_LONG).show();
+
+
+                            Toast.makeText(getApplicationContext(), "Returning sensor data:"+count.toString(), Toast.LENGTH_LONG).show();
                         }
-                });
+                    });
                 }
             }
+
             if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
                 //TO-DO
             }
@@ -76,6 +105,64 @@ public class DataCollectorService extends Service implements SensorEventListener
 
     }
 
+    private void convertDateToTimestamp(Date date){
+        java.util.Date utilDate = new java.util.Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(utilDate);
+        cal.set(Calendar.MILLISECOND, 0);
+        System.out.println(new java.sql.Timestamp(utilDate.getTime()));
+        System.out.println(new java.sql.Timestamp(cal.getTimeInMillis()));
+    }
+
+
+
+    private void publishProgress(String text) {
+        Log.v("ABC", "reporting back from the Random Number Thread");
+        Bundle msgBundle = new Bundle();
+        msgBundle.putString("result", text);
+        Message msg = new Message();
+        msg.setData(msgBundle);
+        myHandler.sendMessage(msg);
+    }
+
+
+
+    public String detectActivity(long lastMilliSeconds){
+        int i;
+        int numberOfSamples = 0;
+        for (i=accelerometer_data.size()-1; i>=0 ; i--) {
+            if (accelerometer_data.get(i).getMagnitude() > 0.3)
+            {
+                numberOfSamples++;
+                continue;
+
+            }
+            else
+            {
+                //Add walking activity to the data structure
+                if (numberOfSamples > 5*5*10) {  //TO BE tested
+                    ActivityRecord obj = new ActivityRecord(new Date(lastMilliSeconds + i * 1000), "Walking");
+                    activityRecordArrayList.add(obj);
+                    myHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+
+                            Toast.makeText(getApplicationContext(), "Wrote activity walking", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    break;
+                }
+                numberOfSamples = 0; //If number of samples are not enough to detect that activity is "Walking"
+
+            }
+
+
+        }
+        //Write to SD Card
+
+        return "Done";
+    }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
@@ -93,8 +180,12 @@ public class DataCollectorService extends Service implements SensorEventListener
     public String test_method(){
         return "JUNK";
     }
-    public float[] getSensorData(){
-        return sensorData;
+    public  float[] getSensorData(){
+//        sensorData[0] = xAccl;
+//        sensorData[1] = yAccl;
+//        sensorData[2] = zAccl;
+//        return  sensorData;
+        return  linear_acceleration;
     }
     public void startSensors(){
 
@@ -103,9 +194,12 @@ public class DataCollectorService extends Service implements SensorEventListener
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         sensorManager.registerListener(this,accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this,gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-
-        currentTime = System.currentTimeMillis() + 1*30*1000; //Half minute. Make it two later
+        sensorData = new float[3];
+        gravity = new float[3];
+        linear_acceleration = new float[4]; //Fourth one for magnitude
+        timeOutInterval = System.currentTimeMillis() + INTERVAL; //Half minute. Make it two later
         accelerometer_data = new ArrayList<SensorDataModel>();
+        activityRecordArrayList = new ArrayList<ActivityRecord>();
 
     }
 
